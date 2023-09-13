d1a55ffec089ab5c46b1fc10947a7efffedaf899
Pranay Tulugu <pranay@althea.systems>
2023-09-13 11:15:54 -0700

    Fix CI tests and general cleanup

--- solidity/contract-deployer.ts
+++ solidity/contract-deployer.ts
@@ -33,13 +33,10 @@ async function deploy() {
   let wallet = new ethers.Wallet(args["eth-privkey"], provider);
 
   var success = false;
-  var tries = 0;
   while (!success) {
     var present = new Date();
     var timeDiff: number = present.getTime() - startTime.getTime();
     timeDiff = timeDiff / 1000
-    console.log(tries);
-    tries++;
     provider.getBlockNumber().then(e => { console.log(e); success = true; }).catch(e => { console.log(e); console.log("Ethereum RPC error, trying again"); })
 
     if (timeDiff > 600) {
@@ -109,43 +106,43 @@ async function deploy() {
   // and cast it to the generated Class in that <contract>.d.ts file
 
   // Read ABI (blockchain API) and bytecode from disk
-  const { abi, bytecode } = getContractArtifacts(erc20_a_path);
-  // Create a contract deployer factory
-  const erc20Factory = new ethers.ContractFactory(abi, bytecode, wallet);
-  // Deploy the contract and cast to the typechain generated class
-  const testERC20 = (await erc20Factory.deploy(overrides)) as TestERC20A;
-  await testERC20.deployed(); // Wait
-  const erc20TestAddress = testERC20.address;
-  console.log("ERC20 deployed at Address - ", erc20TestAddress);
+  // const { abi, bytecode } = getContractArtifacts(erc20_a_path);
+  // // Create a contract deployer factory
+  // const erc20Factory = new ethers.ContractFactory(abi, bytecode, wallet);
+  // // Deploy the contract and cast to the typechain generated class
+  // const testERC20 = (await erc20Factory.deploy(overrides)) as TestERC20A;
+  // await testERC20.deployed(); // Wait
+  // const erc20TestAddress = testERC20.address;
+  // console.log("ERC20 deployed at Address - ", erc20TestAddress);
   // Now testERC20 is ready to use, e.g.testERC20.transfer(from, to, amount)
 
-  const { abi: abi1, bytecode: bytecode1 } = getContractArtifacts(erc20_b_path);
-  const erc20Factory1 = new ethers.ContractFactory(abi1, bytecode1, wallet);
-  const testERC201 = (await erc20Factory1.deploy(overrides)) as TestERC20B;
-  await testERC201.deployed();
-  const erc20TestAddress1 = testERC201.address;
-  console.log("ERC20 deployed at Address - ", erc20TestAddress1);
+  // const { abi: abi1, bytecode: bytecode1 } = getContractArtifacts(erc20_b_path);
+  // const erc20Factory1 = new ethers.ContractFactory(abi1, bytecode1, wallet);
+  // const testERC201 = (await erc20Factory1.deploy(overrides)) as TestERC20B;
+  // await testERC201.deployed();
+  // const erc20TestAddress1 = testERC201.address;
+  // console.log("ERC20 deployed at Address - ", erc20TestAddress1);
 
-  const { abi: abi2, bytecode: bytecode2 } = getContractArtifacts(erc20_c_path);
-  const erc20Factory2 = new ethers.ContractFactory(abi2, bytecode2, wallet);
-  const testERC202 = (await erc20Factory2.deploy(overrides)) as TestERC20C;
-  await testERC202.deployed();
-  const erc20TestAddress2 = testERC202.address;
-  console.log("ERC20 deployed at Address - ", erc20TestAddress2);
+  // const { abi: abi2, bytecode: bytecode2 } = getContractArtifacts(erc20_c_path);
+  // const erc20Factory2 = new ethers.ContractFactory(abi2, bytecode2, wallet);
+  // const testERC202 = (await erc20Factory2.deploy(overrides)) as TestERC20C;
+  // await testERC202.deployed();
+  // const erc20TestAddress2 = testERC202.address;
+  // console.log("ERC20 deployed at Address - ", erc20TestAddress2);
 
-  const { abi: abi3, bytecode: bytecode3 } = getContractArtifacts(erc721_a_path);
-  const erc721Factory1 = new ethers.ContractFactory(abi3, bytecode3, wallet);
-  const testERC721 = (await erc721Factory1.deploy(overrides)) as TestERC721A;
-  await testERC721.deployed();
-  const erc721TestAddress = testERC721.address;
-  console.log("ERC721 deployed at Address - ", erc721TestAddress);
+  // const { abi: abi3, bytecode: bytecode3 } = getContractArtifacts(erc721_a_path);
+  // const erc721Factory1 = new ethers.ContractFactory(abi3, bytecode3, wallet);
+  // const testERC721 = (await erc721Factory1.deploy(overrides)) as TestERC721A;
+  // await testERC721.deployed();
+  // const erc721TestAddress = testERC721.address;
+  // console.log("ERC721 deployed at Address - ", erc721TestAddress);
 
   const { abi: abi4, bytecode: bytecode4 } = getContractArtifacts(althea_db_path);
   const altheadbFactory1 = new ethers.ContractFactory(abi4, bytecode4, wallet);
   const testAltheaDB = (await altheadbFactory1.deploy(overrides)) as AltheaDB;
   await testAltheaDB.deployed();
   const altheaDBTestAddress = testAltheaDB.address;
-  console.log("Althea DB deployed at Address - ", altheaDBTestAddress);
+  console.log("Althea_DB_addr: ", altheaDBTestAddress);
 }
 
 function getContractArtifacts(path: string): { bytecode: string; abi: string } {
